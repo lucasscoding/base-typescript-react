accc396b999eb9308fb4dac175fe74d81043a37b
Lucas Lopes <lucass.code@gmail.com>
2022-06-29 17:02:52 -0300

    chore: add css loader

--- src/presentation/views/hello-view.tsx
+++ src/presentation/views/hello-view.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
-
+import style from './hello.css'
 export class Hello extends React.Component {
   render (): React.ReactNode {
-    return <p>Hello World</p>
+    return <p className={style.color}>Hello World</p>
   }
 }
